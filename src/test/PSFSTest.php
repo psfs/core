b03ed885ce7b280a687cd0ad285326960e6da493
Fran López <flopez@minds.com.es>
2014-07-30 22:10:43 +0200

    Travis.yml

---
+++ src/test/PSFSTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class PSFSTest extends \PHPUnit_Framework_TestCase{
+    public function testBasic()
+    {
+
+    }
+}
---
+++ src/test/PSPFSTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PSFS;
+
+class PSFSTest extends \PHPUnit_Framework_TestCase{
+    
+}
