14a49efaa5fade8a9e916c75768071d50084fc4a
Fran López <fran.lopez84@hotmail.es>
2016-11-28 20:49:56 +0100

    Finally fixed AuthAdminController

--- src/base/types/AuthAdminController.php
+++ src/base/types/AuthAdminController.php
@@ -1,16 +1,22 @@
 <?php
-    namespace PSFS\base\types;
+namespace PSFS\base\types;
 
-    use PSFS\controller\base\Admin;
+use PSFS\base\types\interfaces\AuthInterface;
+use PSFS\controller\base\Admin;
 
-    /**
-     * Class AuthAdminController
-     * @package PSFS\base\types
-     */
-    class AuthAdminController extends AuthController {
-        public function init() {
-            if (!$this->isAdmin()) {
-                Admin::staticAdminLogon();
-            }
+/**
+ * Class AuthAdminController
+ * @package PSFS\base\types
+ */
+class AuthAdminController extends Controller implements AuthInterface
+{
+    use SecureTrait;
+
+    public function init()
+    {
+        parent::init();
+        if (!$this->isAdmin()) {
+            Admin::staticAdminLogon();
         }
     }
+}
