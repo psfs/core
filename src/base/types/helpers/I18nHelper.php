689111e20492864ed4c88df5044d223b018d306a
Fran López <fran.lopez84@hotmail.es>
2017-11-22 13:13:15 +0100

    I18n helper to sanitize

--- src/base/types/helpers/I18nHelper.php
+++ src/base/types/helpers/I18nHelper.php
@@ -127,4 +127,48 @@ class I18nHelper
         }
         return $isI18n;
     }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public static function sanitize($string) {
+        $text = filter_var($string, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+        $text = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $text
+        );
+
+        $text = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $text
+        );
+
+        $text = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $text
+        );
+
+        $text = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $text
+        );
+
+        $text = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $text
+        );
+
+        $text = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C',),
+            $text
+        );
+        return $text;
+    }
 }
