1016bd5825a1e4438ba95ad7ef6dce520c23d2b6
Fran López <fran.lopez@grupotks.com>
2018-09-17 11:28:07 +0200

    Added CustomAuthApi

---
+++ src/base/types/CustomAuthApi.php
@@ -0,0 +1,20 @@
+<?php
+namespace PSFS\base\types;
+
+use PSFS\base\exception\ApiException;
+use PSFS\base\Security;
+
+/**
+ * Class CustomAuthApi
+ * @package PSFS\base\types
+ */
+abstract class CustomAuthApi extends CustomApi {
+
+    public function init()
+    {
+        if(!Security::getInstance()->isLogged()) {
+            throw new ApiException(t('Resource not authorized'), 401);
+        }
+        parent::init();
+    }
+}
