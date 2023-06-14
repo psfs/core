caa7b58da6c4a6dfa2c72c89976a202c71fbecae
Fran López <fran.lopez84@hotmail.es>
2023-06-15 01:12:38 +0200

    Revert "- Rolled back refactor"
    This reverts commit 9f99332ddbb278706418062a099d13503d9f4b93.

--- src/base/types/Api.php
+++ src/base/types/Api.php
@@ -12,6 +12,7 @@ use PSFS\base\Logger;
 use PSFS\base\Request;
 use PSFS\base\Singleton;
 use PSFS\base\types\helpers\ApiHelper;
+use PSFS\base\types\traits\Api\Crud\ApiListTrait;
 use PSFS\base\types\traits\Api\ManagerTrait;
 
 /**
@@ -20,15 +21,11 @@ use PSFS\base\types\traits\Api\ManagerTrait;
  */
 abstract class Api extends Singleton
 {
-    use ManagerTrait;
+    use ManagerTrait, ApiListTrait;
 
-    const API_COMBO_FIELD = '__combo';
     const API_LIST_NAME_FIELD = '__name__';
     const API_FIELDS_RESULT_FIELD = '__fields';
     const API_MODEL_KEY_FIELD = '__pk';
-    const API_ORDER_FIELD = '__order';
-    const API_PAGE_FIELD = '__page';
-    const API_LIMIT_FIELD = '__limit';
     const API_PK_SEPARATOR = '__|__';
 
     const API_ACTION_LIST = 'list';
@@ -41,27 +38,6 @@ abstract class Api extends Singleton
     const HEADER_API_TOKEN = 'X-API-SEC-TOKEN';
     const HEADER_API_LANG = 'X-API-LANG';
     const HEADER_API_FIELDTYPE = 'X-FIELD-TYPE';
-
-    /**
-     * @var \Propel\Runtime\Collection\Collection|\Propel\Runtime\Util\PropelModelPager $list
-     */
-    protected $list;
-
-    /**
-     * @var array $filters
-     */
-    protected $filters = array();
-
-    /**
-     * @Injectable
-     * @var \PSFS\base\dto\Order $order
-     */
-    protected $order;
-
-    /**
-     * @var array $query
-     */
-    protected $query = array();
 
     /**
      * @var string
@@ -101,107 +77,6 @@ abstract class Api extends Singleton
             case 'put': $this->action = self::API_ACTION_PUT; break;
             case 'delete': $this->action = self::API_ACTION_DELETE; break;
             case 'bulk': $this->action = self::API_ACTION_BULK; break;
-        }
-    }
-
-    /**
-     * Hydrate order from request
-     */
-    protected function hydrateOrders()
-    {
-        if (count($this->query)) {
-            Logger::log(static::class . ' gathering query string', LOG_DEBUG);
-            foreach ($this->query as $key => $value) {
-                if ($key === self::API_ORDER_FIELD) {
-                    $orders = json_decode($value, true);
-                    foreach ($orders as $field => $direction) {
-                        $this->order->addOrder($field, $direction);
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Extract pagination values
-     * @return array
-     */
-    protected function extractPagination()
-    {
-        Logger::log(static::class . ' extract pagination start', LOG_DEBUG);
-        $page = array_key_exists(self::API_PAGE_FIELD, $this->query) ? $this->query[self::API_PAGE_FIELD] : 1;
-        $limit = array_key_exists(self::API_LIMIT_FIELD, $this->query) ? $this->query[self::API_LIMIT_FIELD] : 100;
-        Logger::log(static::class . ' extract pagination end', LOG_DEBUG);
-        return array($page, (int)$limit);
-    }
-
-    /**
-     * Add order fields to query
-     * @param ModelCriteria $query
-     * @throws \PSFS\base\exception\ApiException
-     */
-    private function addOrders(ModelCriteria &$query)
-    {
-        Logger::log(static::class . ' extract orders start ', LOG_DEBUG);
-        $orderAdded = FALSE;
-        $tableMap = $this->getTableMap();
-        foreach ($this->order->getOrders() as $field => $direction) {
-            if ($column = ApiHelper::checkFieldExists($tableMap, $field)) {
-                $orderAdded = TRUE;
-                if ($direction === Order::ASC) {
-                    $query->addAscendingOrderByColumn($column->getPhpName());
-                } else {
-                    $query->addDescendingOrderByColumn($column->getPhpName());
-                }
-            }
-        }
-        if (!$orderAdded) {
-            $pks = $this->getPkDbName();
-            foreach(array_keys($pks) as $key) {
-                $query->addAscendingOrderByColumn($key);
-            }
-        }
-        Logger::log(static::class . ' extract orders end', LOG_DEBUG);
-    }
-
-    /**
-     * Add filters fields to query
-     *
-     * @param ModelCriteria $query
-     */
-    protected function addFilters(ModelCriteria &$query)
-    {
-        if (count($this->query) > 0) {
-            $tableMap = $this->getTableMap();
-            foreach ($this->query as $field => $value) {
-                if (self::API_COMBO_FIELD === $field) {
-                    ApiHelper::composerComboField($tableMap, $query, $this->extraColumns, $value);
-                } elseif(!preg_match('/^__/', $field)) {
-                    ApiHelper::addModelField($tableMap, $query, $field, $value);
-                }
-            }
-        }
-    }
-
-    /**
-     * Generate list page for model
-     */
-    private function paginate()
-    {
-        $this->list = null;
-        try {
-            $query = $this->prepareQuery();
-            $this->addFilters($query);
-            $this->checkReturnFields($query);
-            $this->addOrders($query);
-            list($page, $limit) = $this->extractPagination();
-            if ($limit === -1) {
-                $this->list = $query->find($this->con);
-            } else {
-                $this->list = $query->paginate($page, $limit, $this->con);
-            }
-        } catch (\Exception $e) {
-            Logger::log($e->getMessage(), LOG_ERR);
         }
     }
 
@@ -396,16 +271,6 @@ abstract class Api extends Singleton
             $message = t('Bulk insert rolled back');
         }
         return $this->json(new JsonResponse($this->exportList(), $saved, count($this->list), 1, $message), $status);
-    }
-
-    /**
-     * Hydrate data from request
-     */
-    protected function hydrateRequestData()
-    {
-        $request = Request::getInstance();
-        $this->query = array_merge($this->query, $request->getQueryParams());
-        $this->data = array_merge($this->data, $request->getRawData());
     }
 
     private function extractDataWithFormat() {
